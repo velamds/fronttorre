11c16c72e24c1ad5263d2bfdcf8b3392d0cca38b
velamds <velamds@gmail.com>
2020-10-18 12:04:47 -0500

    Updates

--- precache-manifest.e450d6b81e3534c1451eb8de2b9fc81b.js
+++ precache-manifest.e450d6b81e3534c1451eb8de2b9fc81b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53b07e48cc86ddcfc4ebf94c961a4473",
+    "revision": "572a30ca4e48dd015e677f9904f4e648",
     "url": "/fronttorre/index.html"
   },
   {
-    "revision": "2621b6fc33b967fb78c6",
+    "revision": "a4fccb628fde685f8587",
     "url": "/fronttorre/static/css/2.6414bb7b.chunk.css"
   },
   {
-    "revision": "86053b5a34eb846da22a",
+    "revision": "2186aa4e22799ce60258",
     "url": "/fronttorre/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "2621b6fc33b967fb78c6",
-    "url": "/fronttorre/static/js/2.a4f0a6c9.chunk.js"
+    "revision": "a4fccb628fde685f8587",
+    "url": "/fronttorre/static/js/2.24f70e2a.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/fronttorre/static/js/2.a4f0a6c9.chunk.js.LICENSE.txt"
+    "url": "/fronttorre/static/js/2.24f70e2a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "86053b5a34eb846da22a",
-    "url": "/fronttorre/static/js/main.72effc66.chunk.js"
+    "revision": "2186aa4e22799ce60258",
+    "url": "/fronttorre/static/js/main.fad8a231.chunk.js"
   },
   {
     "revision": "82422620da8833982ac5",
